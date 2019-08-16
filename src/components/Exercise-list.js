81c7f9e2c19a69e1e7bf3ba6aceee1a405a4e4ab
feministAF <santiagoreha24@gmail.com>
2019-08-17 01:06:56 +0530

    third commit

--- src/components/Exercise-list.js
+++ src/components/Exercise-list.js
@@ -1,5 +1,26 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
 class ExerciseList extends Component{
+    constructor(props){
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state = {
+            exercises:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/exercises/')
+            .then(response => {
+                this.setState({
+                    exercises:response.data
+                })
+            })
+    }
+
     render(){
         return(
             <div>
